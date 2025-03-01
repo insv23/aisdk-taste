80e9fbd6b22815dc95db78ca921e4f85216060f6
insv <insv23@outlook.com>
2025-03-01 16:52:27 +0800

    07-structured-outputs

---
+++ src/07-structured-outputs/main.ts
@@ -0,0 +1,62 @@
+import { createOpenAICompatible } from "@ai-sdk/openai-compatible";
+import { generateObject } from "ai";
+import { z } from "zod";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const baseURL = process.env.OPENAI_BASE_URL;
+const apiKey = process.env.OPENAI_API_KEY;
+
+if (!baseURL || !apiKey) {
+	let errorMessage =
+		"Error: Missing required environment variables for OpenAI integration.\n";
+
+	if (!baseURL) {
+		errorMessage += "- OPENAI_BASE_URL is not set.\n";
+	}
+
+	if (!apiKey) {
+		errorMessage += "- OPENAI_API_KEY is not set.\n";
+	}
+
+	console.error(errorMessage);
+	process.exit(1);
+}
+
+const aiClient = createOpenAICompatible({
+	name: "aiClient",
+	baseURL,
+	apiKey,
+});
+
+const model = aiClient("gpt-4o-mini");
+
+const schema = z.object({
+	recipe: z.object({
+		name: z.string().describe("The title of the ricipe"),
+		ingredients: z
+			.array(z.object({ name: z.string(), amount: z.string() }))
+			.describe("The ingredients needed for the recipe"),
+		steps: z.array(z.string()).describe("The steps to make the recipe"),
+	}),
+});
+
+export const createRecipe = async (prompt: string) => {
+	const { object } = await generateObject({
+		model,
+		schema,
+		prompt,
+		schemaName: "Recipe",
+		system:
+			"You are helping a user create a recipe." +
+			"Use British English variants of ingredient names," +
+			"like Coriander over Cilantro.",
+	});
+
+	return object.recipe;
+};
+
+const recipe = await createRecipe("如何制作啤酒鸭?");
+
+console.dir(recipe, { depth: null });
